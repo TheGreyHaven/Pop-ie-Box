945e2c4cd9a13e6c6ba078e41443ab22eb052461
Haven Giguere <haven.giguere@gmail.com>
2017-02-17 15:10:20 -0800

    working arrows

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,3 +1,30 @@
 
 
+$(document).ready(function(){
+var currentIndex = 1,
+  items = $('#boxes > div'),
+  itemAmt = items.length;
 
+function cycleItems() {
+  var item = $('#boxes > div').eq(currentIndex);
+  items.hide();
+  item.css('display','inline-block');
+}
+
+$('.next').click(function() {
+  currentIndex += 1;
+  if (currentIndex > itemAmt - 1) {
+    currentIndex = 0;
+  }
+  cycleItems();
+});
+
+
+$('.prev').click(function() {
+  currentIndex -= 1;
+  if (currentIndex < 0) {
+    currentIndex = itemAmt - 1;
+  }
+  cycleItems();
+});
+});
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 
-// window.addEventListner("load", drag_start, false);
+
 
 function _(id) {
 		return document.getElementById(id);
@@ -17,9 +17,9 @@ function drag_drop(event) {
 	//here we are retreiving the data that we set earlier in the drag_start function. This gives us the element id that is being dropped into the drop zone. 
 	event.target.appendChild( _(elem_id) );
 	//event.target is the drop zone object. appendChild is adding the element to it (vie it's id) now that element lives in the drop zone. 
-	// _(elem_id).removeAttribute("draggable");
+	_(elem_id).removeAttribute("draggable");
 	//this makes it so that the element which is now in place in the drop zone can't be draged back out. 
-	// _(elem_id).style.cursor = "default";
+	_(elem_id).style.cursor = "default";
 	//now the curser is back to it's default and no longer telling people that the object can be moved. 
 }
 
@@ -29,3 +29,4 @@ function drag_drop(event) {
 // 		alert(_("canvas").children[i].id+" is in the drop zone");
 // 	}
 // }
+
