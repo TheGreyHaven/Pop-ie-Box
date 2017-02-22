271320d8ce9b5dd060daa082170197af7f0cb76b
Haven Giguere <haven.giguere@gmail.com>
2017-02-21 21:33:37 -0800

    image changes

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,30 +1,47 @@
 
 
 $(document).ready(function(){
-var currentIndex = 1,
-  items = $('#boxes > div'),
-  itemAmt = items.length;
+	var currentIndex = 1,
+	  items = $('#boxes > div'),
+	  itemAmt = items.length;
 
-function cycleItems() {
-  var item = $('#boxes > div').eq(currentIndex);
-  items.hide();
-  item.css('display','inline-block');
-}
+	function cycleItems() {
+	  var item = $('#boxes > div').eq(currentIndex);
+	  items.hide();
+	  item.css('display','inline-block');
+	}
 
-$('.next').click(function() {
-  currentIndex += 1;
-  if (currentIndex > itemAmt - 1) {
-    currentIndex = 0;
-  }
-  cycleItems();
+	$('.next').click(function() {
+	  currentIndex += 1;
+	  if (currentIndex > itemAmt - 1) {
+	    currentIndex = 0;
+	  }
+	  cycleItems();
+	});
+
+
+	$('.prev').click(function() {
+	  currentIndex -= 1;
+	  if (currentIndex < 0) {
+	    currentIndex = itemAmt - 1;
+	  }
+	  cycleItems();
+	});
+
+// color, season and flower type selectors
+	// $("#colorList > li").on('click', function(){
+	$("#colorList > a").each(
+    function() {
+        var classes = this.classList;
+           if ($('#boxes img').hasClass(classes)){
+                $(this).addClass('hidden');
+            }
+        
+    });​
+
+	
+
+
 });
 
 
-$('.prev').click(function() {
-  currentIndex -= 1;
-  if (currentIndex < 0) {
-    currentIndex = itemAmt - 1;
-  }
-  cycleItems();
-});
-});
